e41a805736c703424dea408157c60190f110e5d4
OrlandoCJ34 <OrlandoCJ34@gmail.com>
2022-11-26 00:53:35 -0600

    Actualizacion-Login-Github

--- app/src/main/java/mx/cdhidalgo/tecnm/shoppit/Dashboard.kt
+++ app/src/main/java/mx/cdhidalgo/tecnm/shoppit/Dashboard.kt
@@ -1,11 +1,27 @@
 package mx.cdhidalgo.tecnm.shoppit
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class Dashboard : AppCompatActivity() {
+    lateinit var btnLogout:Button
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
+
+        //findview
+        btnLogout = findViewById(R.id.Logout)
+         btnLogout.setOnClickListener{
+             Firebase.auth.signOut()
+             intent = Intent(this,Login::class.java)
+             startActivity(intent)
+         }
     }
 }
--- app/src/main/java/mx/cdhidalgo/tecnm/shoppit/Login.kt
+++ app/src/main/java/mx/cdhidalgo/tecnm/shoppit/Login.kt
@@ -1,39 +1,44 @@
 package mx.cdhidalgo.tecnm.shoppit
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
 import android.widget.RelativeLayout
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.*
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
+
 class Login : AppCompatActivity() {
     //variables globales
-
+    private lateinit var UserF: FirebaseUser
     private lateinit var auth: FirebaseAuth
     private lateinit var btnLg: RelativeLayout
 
     private lateinit var btnRg: TextView
     private lateinit var LogGoogle : ImageView
-    private lateinit var LogTwitter : ImageView
+    private lateinit var LogGit : ImageView
     // Constante
     private val GSignIn = 100
+    val provider = OAuthProvider.newBuilder("github.com")
 
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        //github
+
+        auth = FirebaseAuth.getInstance()
 
         auth = Firebase.auth
         //Acceso a la base de datos CloudFirestore
@@ -43,8 +48,10 @@ class Login : AppCompatActivity() {
         btnLg = findViewById(R.id.btnLogin)
         btnRg = findViewById(R.id.btnRegistro)
         LogGoogle = findViewById(R.id.btnG)
+        LogGit = findViewById(R.id.btnGit)
 
 
+        //btn inicio de sesion Google
         LogGoogle.setOnClickListener{
             val googlecon = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestIdToken(getString(R.string.default_web_client_id))
@@ -55,13 +62,13 @@ class Login : AppCompatActivity() {
             startActivityForResult(googleClient.signInIntent, GSignIn)
         }
 
-
-
-
-        btnLg.setOnClickListener {
-            val intent = Intent(this, Dashboard::class.java)
+        //btn Inicio de sesion Git
+        LogGit.setOnClickListener {
+            val intent = Intent(this, GithubAuth::class.java)
             startActivity(intent)
+
         }
+
 
         btnRg.setOnClickListener {
             val intent = Intent(this, Registro::class.java)
@@ -110,4 +117,6 @@ class Login : AppCompatActivity() {
         }
 
     }
+
+
 }
